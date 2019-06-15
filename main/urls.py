152c7f7391aea2d805d0fdd19f127936c83021d4
Carl Riis <carl.aagot.riis@gmail.com>
2019-06-15 19:15:45 +0200

    finished static pages

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 app_name = 'main'
 
 urlpatterns = [
-	path("", views.home)
+	path("", views.home),
+	path("about-me/", views.about_me),
+	path("projects/", views.projects),
+	path("contact/", views.contact),
 ]
--- main/views.py
+++ main/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render_to_response
 def home(request):
 	return render_to_response("home.html")
 
-# Create your views here.
+def about_me(request):
+	return render_to_response("about_me.html")
+
+def projects(request):
+	return render_to_response("projects.html")
+
+def contact(request):
+	return render_to_response("contact.html")
