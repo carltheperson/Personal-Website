867d7409184bcd98eaa918068468c4b390f4c523
Carl Riis <carl.aagot.riis@gmail.com>
2020-08-13 15:18:03 +0200

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./css/Header.css";
 
 const burger = require("../media/burger.svg");
 
 export default () => {
+  const [looking, setLooking] = useState<boolean>();
+  const [locked, setLocked] = useState<boolean>();
+
+  const getMenuClass = () => {
+    if (looking === undefined) return "hidden";
+    return looking || locked ? "active" : "inactive";
+  };
+
   return (
-    <div>
-      <h1>Carl Riis</h1>
-      {/* <img src={burger} alt="burger menu" /> */}
+    <div className="header">
+      <h1 className="logo">Carl Riis</h1>
+      <ul
+        className="menu"
+        onMouseEnter={() => setLooking(true)}
+        onMouseLeave={() => setLooking(false)}
+      >
+        <li>
+          <img
+            className="burger"
+            src={burger}
+            alt="burger menu"
+            onClick={() => setLocked(!locked)}
+          />
+        </li>
+        <li className={getMenuClass()}>
+          <a>Welcome</a>
+        </li>
+        <li className={getMenuClass()}>
+          <a>About me</a>
+        </li>
+        <li className={getMenuClass()}>
+          <a>Contact me</a>
+        </li>
+      </ul>
     </div>
   );
 };
