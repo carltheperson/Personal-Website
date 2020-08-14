8da575732e06580a4bf9beaf7e94f48736a9128c
Carl Riis <carl.aagot.riis@gmail.com>
2020-08-14 07:45:07 +0200

    About me

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Header />
       <Welcome />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import FileSaver from "file-saver";
+
+import "./css/About.css";
+
+const me = require("../media/me.png");
+const outline = require("../media/outline.png");
+const pdfIcon = require("../media/pdf-icon.svg");
+
+export default () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="me">
+          <img src={outline} alt="outline" />
+          <img src={me} alt="me" className="me-img" />
+        </div>
+        <div className="description">
+          <h1>About me</h1>
+          <p>
+            <span>
+              I’m an 18 year old programmer who is very interested in
+              technology. I’m experienced in web development and things related
+              to DevOps.
+            </span>
+            <br />
+            <br />
+            <span>
+              To learn more about my experience and skills you can refer to my
+              CV, which you can download right here:
+            </span>
+          </p>
+          <div
+            className="download-container"
+            onClick={() =>
+              FileSaver.saveAs(
+                process.env.PUBLIC_URL + "/files/test.txt",
+                "test.txt"
+              )
+            }
+          >
+            <img src={pdfIcon} alt="Pdf icon" className="pdf-icon" />
+            <div>
+              <p className="download-text">Download CV</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
